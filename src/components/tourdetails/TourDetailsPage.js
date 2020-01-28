36cd4c19f1195d81f3570be4551859eb2cc67a63
Michael Anokye <michaelanokyej@yahoo.com>
2020-01-28 06:34:25 -0500

    added styles to tour page

--- src/components/tourdetails/TourDetailsPage.js
+++ src/components/tourdetails/TourDetailsPage.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { format } from "date-fns";
-import toursAndUserContext from '../context/toursAndUserContext.js';
+import toursAndUserContext from "../context/toursAndUserContext.js";
+import ProfilePicture from "./profilePic.jpeg";
+import "./TourDetailsPage.css"
+
 // import RemoveButton from "../removebutton/RemoveButton";
 
 class TourDetailsPage extends React.Component {
   static contextType = toursAndUserContext;
 
-  render () {
+  render() {
     const id = this.props.match.params.tourId;
     const tour = this.context.tours.find(el => el.id === id);
     // console.log("id", id)
     // console.log("context", this.context)
-    // console.log("selected tour", tour)
+    console.log("selected tour", tour);
 
     if (!tour) {
       return (
@@ -24,35 +27,53 @@ class TourDetailsPage extends React.Component {
     return (
       <section className="tourPage">
         <div className="tour">
-          <div className="tour__title">
+          {/* <div className="tour__title">
           <h2 >
             <Link to={`/tours/${tour.id}`}>{tour.name}</Link>
           </h2>
-          </div> 
-          <div className="tourImage">
-          <img src={tour.img} alt="tour"/>
+          </div>  */}
+          <div className="tour__image-container">
+            <img src={tour.img} alt="tour" />
           </div>
-          <div className="tour__description">
-          <p>{tour.city},{tour.state}</p>
-          </div>
-
-          
-        <div className="NotePageMain__content">
-          {tour.description.split(/\n \r|\n/)}
-        </div>
-        <div className="Note__dates">
-            <div className="Note__dates-modified">
+          <div className="tour__info-guide__info">
+            <div className="tour-name">
+              <span className="titles">{tour.name}</span>
+              <span>
+                <p className="text--medium tour__location">
+                  {tour.city}, {tour.state}
+                </p>
+              </span>
+            </div>
+            <div className="guide__info">
+              <div className="guide__img-container">
+                <img src={ProfilePicture} alt="user" />
+              </div>
+              <div className="guide-username text--medium">{tour.guide_username}</div>
+            </div>
+            <div className="tour__rules">
+              <div className="text--medium">Max Tourists: {tour.max_tourists}</div>
+              <div className="policies">
+                <div className="titles">CHECK-IN POLICIES</div>
+                <div className="text--medium">{tour.policies}</div>
+              </div>
+            </div>
+            <div className="tour_description text--medium">
+              {tour.description.split(/\n \r|\n/)}
+            </div>
+            <div className="tour__posted text--medium">
               Posted on{" "}
               <span className="Date">
                 {format(new Date(tour.posted), "dd MMM yyyy")}
               </span>
             </div>
           </div>
+          {/*
+          If guide userName === to logged in userName show <RemoveButton id={tour.id} /> 
+          Else show book tour*/}
         </div>
-        {/* <RemoveButton id={tour.id} /> */}
-
+        
       </section>
-    )
+    );
   }
 }
 
